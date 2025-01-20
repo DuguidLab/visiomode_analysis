a7e87b3ae210b23a0c5fe0731c533914163140df
sophiegholland <s1934312@ed.ac.uk>
2025-01-20 11:41:36 +0000

    Calculate the mean and median RT for a single session Fixes #2

--- src/visiomode_analysis/session/metrics.py
+++ src/visiomode_analysis/session/metrics.py
@@ -46,12 +46,50 @@ def d_prime(df, session_type):
     # Return d' 
     return d_prime
 
-# Calculate mean RT for all trial types
-mean_rt_hits = df[(df.outcome == "correct") & (df.response.notnull()) & (df.correction == False)]["response_time"].mean()
 
-mean_rt_false_alarms = df[(df.outcome == "incorrect") & (df.response.notnull()) & (df.correction == False)]["response_time"].mean()
+def rt_metric(df, trial_type, metric_type):
+    '''Defines a function to calculate mean or median RT for different trial types
+    
+    Args:
+        df: Pandas dataframe with session data.
+        trial_type: hit or false_alarm
+        metric type: mean_rt or median_rt
+    
+    Output: 
+        float representing the mean/median RT for a all hit/FA trials in a session
+    
+    Example: 
+        hit_mean_RT = rt_metric(df, "hit", "mean"_rt)'''
+    
+    #calculate mean RT for each trial type
+    mean_rt_hits = df[(df.outcome == "correct") & (df.response.notnull()) & (df.correction == False)]["response_time"].mean()
 
-# Calculate median RT for all trial types
-median_rt_hits = df[(df.outcome == "correct") & (df.response.notnull()) & (df.correction == False)]["response_time"].median()
+    mean_rt_fas = df[(df.outcome == "incorrect") & (df.response.notnull()) & (df.correction == False)]["response_time"].mean()
 
-median_rt_false_alarms = df[(df.outcome == "incorrect") & (df.response.notnull()) & (df.correction == False)]["response_time"].median()
+    mean_rt_both = df[(df.response.notnull()) & (df.correction == False)]["response_time"].mean()
+
+    # calculate median RT for each trial type
+    median_rt_hits = df[(df.outcome == "correct") & (df.response.notnull()) & (df.correction == False)]["response_time"].median()
+
+    median_rt_fas = df[(df.outcome == "incorrect") & (df.response.notnull()) & (df.correction == False)]["response_time"].median()
+
+    median_rt_both = df[(df.response.notnull()) & (df.correction == False)]["response_time"].median()
+  
+    if trial_type == "hits":
+        if metric_type == "mean":
+            rt_metric = mean_rt_hits
+        else:
+            rt_metric = median_rt_hits
+    elif trial_type == "false_alarms":
+        if metric_type == "mean":
+            rt_metric = mean_rt_fas
+        else:
+            rt_metric = median_rt_fas
+    else:
+        if metric_type == "mean":
+            rt_metric = mean_rt_both
+        else:
+            rt_metric = median_rt_both
+
+    # return desired metric
+    return rt_metric
