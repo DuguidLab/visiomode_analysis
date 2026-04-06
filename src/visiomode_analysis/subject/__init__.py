9e7c0ad74fb02e56081ce326401d7b7bd12b781a
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2026-04-06 15:01:23 +0100

    add collate session function

--- src/visiomode_analysis/subject/__init__.py
+++ src/visiomode_analysis/subject/__init__.py
@@ -19,9 +19,53 @@
 #  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 
-
+import os
 import click
+import glob
+import pandas as pd
+
+import visiomode_analysis.session as session
 
 
 @click.command("subject")
-def subject_cmd(): ...
+@click.argument(
+    "directory",
+    type=click.Path(exists=True, dir_okay=True, file_okay=False),
+)
+@click.option(
+    "-o",
+    "--output-dir",
+    type=click.Path(dir_okay=True),
+    default=".",
+    help="Output directory for report and trials files.",
+)
+def subject_cmd(**kwargs): ...
+
+
+def collate_sessions(directory, output_dir: str | None) -> pd.DataFrame | tuple[pd.DataFrame, str]:
+    session_files = glob.glob(f"{directory}{os.sep}*trials.csv")
+
+    if not session_files:
+        raise FileNotFoundError(f"No trials.csv files found in {directory}, did you forget to preprocess?")
+
+    subject_sessions = []
+    for session_file in session_files:
+        subject_sessions.append(session.summary(session_file))
+
+    subject_df = pd.DataFrame(subject_sessions)
+
+    subject_df = subject_df.sort_values("session_date").reset_index(drop=True)
+    subject_df["session_id"] = subject_df.index + 1
+    subject_df["task_session"] = subject_df.groupby(["animal_id", "protocol"], sort=False)["session_id"].rank(
+        ascending=True
+    )
+
+    metadata = {
+        "animal_id": subject_df["animal_id"][0],
+        "experiment": subject_df["experiment"][0],
+    }
+
+    if output_dir:
+        out_path = f"{output_dir}{os.sep}sub-{metadata.get('animal_id')}_exp-{metadata.get('experiment')}_behaviour-summary.csv"
+        subject_df.to_csv(out_path)
+    return subject_df
